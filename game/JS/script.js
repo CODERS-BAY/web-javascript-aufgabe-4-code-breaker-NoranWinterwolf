8ffe8acbbd5c549970ee06e5c28e79724bad228a
Noran <matschiff@gmx.de>
2020-09-24 20:41:22 +0200

    styling

--- game/JS/script.js
+++ game/JS/script.js
@@ -129,33 +129,99 @@ function activateButtons() {
         buttons[i].disabled = false;
     }
 }
-document.addEventListener("keydown", function () { buttons[0].focus(); buttons[0].blur(); })
+document.addEventListener("keydown", function (event) { 
+    buttons[0].focus(); buttons[0].blur();
+
+    switch (event.key) {
+        case '1': buttons[0].style.color = "aqua";
+        buttons[0].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
+            break;
+        case '2': buttons[1].style.color = "aqua";
+        buttons[1].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
+            break;
+        case '3': buttons[2].style.color = "aqua";
+        buttons[2].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
+            break;
+        case '4': buttons[3].style.color = "aqua";
+        buttons[3].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
+            break;
+        case '5': buttons[4].style.color = "aqua";
+        buttons[4].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
+            break;
+        case '6': buttons[5].style.color = "aqua";
+        buttons[5].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
+            break;
+        case '7': buttons[6].style.color = "aqua";
+        buttons[6].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
+            break;
+        case '8': buttons[7].style.color = "aqua";
+        buttons[7].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
+            break;
+        case '9': buttons[8].style.color = "aqua";
+        buttons[8].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
+            break;
+        case ' ':
+        case '#': buttons[9].style.color = "aqua";
+        buttons[9].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
+            break;
+        case '0': buttons[10].style.color = "aqua";
+        buttons[10].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
+            break;
+        case 'Enter': buttons[11].style.color = "aqua";
+        buttons[11].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
+            break;
+    }
+ })
 document.addEventListener("keyup", function (event) {
     switch (event.key) {
         case '1': buttons[0].click();
+        buttons[0].style.color = "rgb(61, 69, 78)";
+        buttons[0].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
             break;
         case '2': buttons[1].click();
+        buttons[1].style.color = "rgb(61, 69, 78)";
+        buttons[1].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
             break;
         case '3': buttons[2].click();
+        buttons[2].style.color = "rgb(61, 69, 78)";
+        buttons[2].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
             break;
         case '4': buttons[3].click();
+        buttons[3].style.color = "rgb(61, 69, 78)";
+        buttons[3].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
             break;
         case '5': buttons[4].click();
+        buttons[4].style.color = "rgb(61, 69, 78)";
+        buttons[4].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
             break;
         case '6': buttons[5].click();
+        buttons[5].style.color = "rgb(61, 69, 78)";
+        buttons[5].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
             break;
         case '7': buttons[6].click();
+        buttons[6].style.color = "rgb(61, 69, 78)";
+        buttons[6].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
             break;
         case '8': buttons[7].click();
+        buttons[7].style.color = "rgb(61, 69, 78)";
+        buttons[7].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
             break;
         case '9': buttons[8].click();
+        buttons[8].style.color = "rgb(61, 69, 78)";
+        buttons[8].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
             break;
         case ' ':
         case '#': buttons[9].click();
+        buttons[9].style.color = "rgb(61, 69, 78)";
+        buttons[9].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
             break;
         case '0': buttons[10].click();
+        buttons[10].style.color = "rgb(61, 69, 78)";
+        buttons[10].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
             break;
         case 'Enter': buttons[11].click();
+        buttons[11].style.color = "rgb(61, 69, 78)";
+        buttons[11].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
             break;
     }
 });
