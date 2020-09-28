b0b01438ac60339a139cd0c519ee7d904cbdeaea
Noran <matschiff@gmx.de>
2020-09-28 08:37:29 +0200

    add underline for user input

--- game/JS/script.js
+++ game/JS/script.js
@@ -29,6 +29,7 @@ function numtest(e) {
 
 function input(e) {
     let number = parseInt(e.target.innerHTML);
+
     userInput[numIndex] = number;
     if (numIndex < 2) {
         numIndex++;
@@ -36,7 +37,18 @@ function input(e) {
     else {
         numIndex = 0;
     }
-    numberField.innerHTML = userInput[0] + " - " + userInput[1] + " - " + userInput[2];
+    switch (numIndex) {
+        case 0:
+            numberField.innerHTML = "<span style='text-decoration:underline;'>" + userInput[0] + "</span> - <span style='text-decoration:none;'>" + userInput[1] + "</span> - <span style='text-decoration:none;'>" + userInput[2] + "</span>"
+            break;
+        case 1:
+            numberField.innerHTML = "<span style='text-decoration:none;'>" + userInput[0] + "</span> - <span style='text-decoration:underline;'>" + userInput[1] + "</span> - <span style='text-decoration:none;'>" + userInput[2] + "</span>"
+            break;
+        case 2:
+            numberField.innerHTML = "<span style='text-decoration:none;'>" + userInput[0] + "</span> - <span style='text-decoration:none;'>" + userInput[1] + "</span> - <span style='text-decoration:underline;'>" + userInput[2] + "</span>"
+            break;
+    }
+    //numberField.innerHTML = userInput[0] + " - " + userInput[1] + " - " + userInput[2];
 }
 
 function gameAction(e) {
@@ -53,7 +65,7 @@ function gameAction(e) {
 function checkCode() {
     let outputString = "";
     let rightNumbers = 0;
-    numberField.innerHTML = "0 - 0 - 0";
+    numberField.innerHTML = "<span style='text-decoration:underline;'>" + userInput[0] + "</span> - <span style='text-decoration:none;'>" + userInput[1] + "</span> - <span style='text-decoration:none;'>" + userInput[2] + "</span>"
     numIndex = 0;
     outputString += "<span ";
     for (i = 0; i < code.length; i++) {
@@ -99,7 +111,7 @@ function startGame() {
     tries = 0;
     output.innerHTML = "";
     alarm.style.display = "none";
-    numberField.innerHTML = "0 - 0 - 0";
+    numberField.innerHTML = "<span style='text-decoration:underline;'>" + userInput[0] + "</span> - <span style='text-decoration:none;'>" + userInput[1] + "</span> - <span style='text-decoration:none;'>" + userInput[2] + "</span>";
     buttons[9].style.boxShadow = "none";
     buttons[9].style.backgroundColor = "grey";
     activateButtons();
@@ -129,99 +141,99 @@ function activateButtons() {
         buttons[i].disabled = false;
     }
 }
-document.addEventListener("keydown", function (event) { 
+document.addEventListener("keydown", function (event) {
     buttons[0].focus(); buttons[0].blur();
 
     switch (event.key) {
         case '1': buttons[0].style.color = "aqua";
-        buttons[0].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
+            buttons[0].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
             break;
         case '2': buttons[1].style.color = "aqua";
-        buttons[1].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
+            buttons[1].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
             break;
         case '3': buttons[2].style.color = "aqua";
-        buttons[2].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
+            buttons[2].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
             break;
         case '4': buttons[3].style.color = "aqua";
-        buttons[3].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
+            buttons[3].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
             break;
         case '5': buttons[4].style.color = "aqua";
-        buttons[4].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
+            buttons[4].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
             break;
         case '6': buttons[5].style.color = "aqua";
-        buttons[5].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
+            buttons[5].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
             break;
         case '7': buttons[6].style.color = "aqua";
-        buttons[6].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
+            buttons[6].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
             break;
         case '8': buttons[7].style.color = "aqua";
-        buttons[7].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
+            buttons[7].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
             break;
         case '9': buttons[8].style.color = "aqua";
-        buttons[8].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
+            buttons[8].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
             break;
         case ' ':
         case '#': buttons[9].style.color = "aqua";
-        buttons[9].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
+            buttons[9].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
             break;
         case '0': buttons[10].style.color = "aqua";
-        buttons[10].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
+            buttons[10].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
             break;
         case 'Enter': buttons[11].style.color = "aqua";
-        buttons[11].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
+            buttons[11].style.textShadow = "0 0 5px aqua, 0 0 10px aqua";
             break;
     }
- })
+})
 document.addEventListener("keyup", function (event) {
     switch (event.key) {
         case '1': buttons[0].click();
-        buttons[0].style.color = "rgb(61, 69, 78)";
-        buttons[0].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
+            buttons[0].style.color = "rgb(61, 69, 78)";
+            buttons[0].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
             break;
         case '2': buttons[1].click();
-        buttons[1].style.color = "rgb(61, 69, 78)";
-        buttons[1].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
+            buttons[1].style.color = "rgb(61, 69, 78)";
+            buttons[1].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
             break;
         case '3': buttons[2].click();
-        buttons[2].style.color = "rgb(61, 69, 78)";
-        buttons[2].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
+            buttons[2].style.color = "rgb(61, 69, 78)";
+            buttons[2].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
             break;
         case '4': buttons[3].click();
-        buttons[3].style.color = "rgb(61, 69, 78)";
-        buttons[3].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
+            buttons[3].style.color = "rgb(61, 69, 78)";
+            buttons[3].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
             break;
         case '5': buttons[4].click();
-        buttons[4].style.color = "rgb(61, 69, 78)";
-        buttons[4].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
+            buttons[4].style.color = "rgb(61, 69, 78)";
+            buttons[4].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
             break;
         case '6': buttons[5].click();
-        buttons[5].style.color = "rgb(61, 69, 78)";
-        buttons[5].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
+            buttons[5].style.color = "rgb(61, 69, 78)";
+            buttons[5].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
             break;
         case '7': buttons[6].click();
-        buttons[6].style.color = "rgb(61, 69, 78)";
-        buttons[6].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
+            buttons[6].style.color = "rgb(61, 69, 78)";
+            buttons[6].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
             break;
         case '8': buttons[7].click();
-        buttons[7].style.color = "rgb(61, 69, 78)";
-        buttons[7].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
+            buttons[7].style.color = "rgb(61, 69, 78)";
+            buttons[7].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
             break;
         case '9': buttons[8].click();
-        buttons[8].style.color = "rgb(61, 69, 78)";
-        buttons[8].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
+            buttons[8].style.color = "rgb(61, 69, 78)";
+            buttons[8].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
             break;
         case ' ':
         case '#': buttons[9].click();
-        buttons[9].style.color = "rgb(61, 69, 78)";
-        buttons[9].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
+            buttons[9].style.color = "rgb(61, 69, 78)";
+            buttons[9].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
             break;
         case '0': buttons[10].click();
-        buttons[10].style.color = "rgb(61, 69, 78)";
-        buttons[10].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
+            buttons[10].style.color = "rgb(61, 69, 78)";
+            buttons[10].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
             break;
         case 'Enter': buttons[11].click();
-        buttons[11].style.color = "rgb(61, 69, 78)";
-        buttons[11].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
+            buttons[11].style.color = "rgb(61, 69, 78)";
+            buttons[11].style.textShadow = "0 -1px 1px #66666671, 0 1px 1px #ffffff86";
             break;
     }
 });
